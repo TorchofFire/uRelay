2aac9c634ba75d60b21b03bc08025564939cccb7
Torch <mistermiland@gmail.com>
2024-11-08 19:10:52 -0700

    added to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,8 +10,8 @@ export default tseslint.config(
     languageOptions: {
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+        tsconfigRootDir: import.meta.dirname
+      }
     },
     rules: {
       quotes: ['error', 'single', { avoidEscape: true }],
@@ -19,15 +19,22 @@ export default tseslint.config(
       'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
       'semi': ['error', 'always'],
       'no-useless-return': 'error',
+      'comma-dangle': ['error', 'never'],
+      'arrow-parens': ['error', 'as-needed'],
+      'func-names': ['error', 'as-needed'],
+      'no-empty': ['error', { 'allowEmptyCatch': true }],
+      'space-in-parens': ['error', 'never'],
+      'array-bracket-spacing': ['error', 'never'],
+      'object-curly-spacing': ['error', 'always'],
       '@typescript-eslint/no-invalid-void-type': 'off',
       '@typescript-eslint/no-misused-promises': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-namespace': 'off'
-    },
+    }
   },
   {
     files: ['eslint.config.mjs'],
-    ...tseslint.configs.disableTypeChecked,
-  },
+    ...tseslint.configs.disableTypeChecked
+  }
 );
